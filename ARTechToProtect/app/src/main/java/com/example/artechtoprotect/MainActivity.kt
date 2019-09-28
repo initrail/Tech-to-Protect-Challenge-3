214ce269ffc633ec9aff7929f6cd5162e4cce43e
Su-ryun <dlee67@msudenver.edu>
2019-09-27 23:30:07 -0600

    Wrote permissions.

--- ARTechToProtect/app/src/main/java/com/example/artechtoprotect/MainActivity.kt
+++ ARTechToProtect/app/src/main/java/com/example/artechtoprotect/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.example.artechtoprotect
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var ENABLE_CAMERA: Int = 111;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermissions();
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            ENABLE_CAMERA -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+
+                } else {
+
+                }
+                return
+            }
+
+            else -> {
+                // fall through.
+            }
+        }
+    }
+
+    fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA),
+                ENABLE_CAMERA)
+        } else {
+            // Permission has already been granted.
+        }
     }
 }
