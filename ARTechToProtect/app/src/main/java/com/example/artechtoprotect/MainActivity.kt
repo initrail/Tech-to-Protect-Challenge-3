86386b256ce237ab8ab91b9badf58e1c530372b2
Su-ryun <dlee67@msudenver.edu>
2019-09-28 10:30:22 -0600

    AR fragment is now there.

--- ARTechToProtect/app/src/main/java/com/example/artechtoprotect/MainActivity.kt
+++ ARTechToProtect/app/src/main/java/com/example/artechtoprotect/MainActivity.kt
@@ -4,9 +4,16 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.os.HandlerCompat.postDelayed
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         checkPermissions();
+        maybeEnableArButton();
+        checkARInstall();
     }
 
     override fun onRequestPermissionsResult(
@@ -48,6 +57,34 @@ class MainActivity : AppCompatActivity() {
                 ENABLE_CAMERA)
         } else {
             // Permission has already been granted.
+        }
+    }
+
+
+    fun maybeEnableArButton() {
+        val availability = ArCoreApk.getInstance().checkAvailability(this)
+        if (availability.isTransient) {
+            // Re-query at 5Hz while compatibility is checked in the background.
+            Handler().postDelayed(Runnable { maybeEnableArButton() }, 200)
+        }
+        if (availability.isSupported) {
+            Log.i("dhl", "AR supported.");
+        } else { // Unsupported or unknown.
+            Log.i("dhl", "AR not supported for the phone.");
+        }
+    }
+
+    fun checkARInstall() {
+        try {
+            when (ArCoreApk.getInstance().requestInstall(this, true)) {
+                ArCoreApk.InstallStatus.INSTALLED -> {
+
+                }
+            }
+        } catch (e: UnavailableUserDeclinedInstallationException) {
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                .show();
+            return;
         }
     }
 }
